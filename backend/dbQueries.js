94d6cd68e3897f229e10226a95ed1c472770bdc9
Saravanan <jvs.ravanan@gmail.com>
2020-06-27 14:30:36 +0530

    current user vs all users average same date

--- backend/dbQueries.js
+++ backend/dbQueries.js
@@ -12,7 +12,7 @@ let pushSession = function(req,res) {
         console.log('connected')
       }
     })
-    client.query("insert into sessions(session_data, profile_id) values ($1,$2);",[req.body.session_data, req.body.profile_id]).then(()=>client.end())
+    client.query("insert into sessions(session_data, profile_id, sid, date, current) values ($1,$2,$3,$4,$5);",[req.body.session_data, req.body.profile_id, req.body.sid, req.body.date, req.body.current]).then(()=>client.end())
     .then((response)=>{res.status(200).json({message: "Data entered"}); client.end()})
     .catch((error)=>{res.status(400).json(error); console.log(error); client.end()})
   }
@@ -77,15 +77,35 @@ async function getUserAverage(req,res) {
   .then((response)=>{resp.totalAverage = (response.rows[0].avg); })
   .catch((err)=>{res.status(400).json(err); console.log(err); })   
   client.end();
-  // console.log(resp)
-  // res.status(200).json(resp)
   return resp
-
 }
+
+async function getUserAverageDate(req,res) {
+  var client = new Client();
+ client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+  await client.query("select avg(cast(session_data -> $1 as float)) from sessions where profile_id = $2 AND date = $3;",[req.params.leg, req.params.pid, req.params.date])
+  .then((response)=>{resp.userAverage = (response.rows[0].avg); })
+  .catch((err)=>{res.status(400).json(err); console.log(err); })   
+  
+  await client.query("select avg(cast(session_data -> $1 as float)) from sessions where date = $2;",[req.params.leg, req.params.date])
+  .then((response)=>{resp.totalAverage = (response.rows[0].avg); })
+  .catch((err)=>{res.status(400).json(err); console.log(err); })   
+  client.end();
+  return resp
+}
+
+
 module.exports={
     pushSession:pushSession,
     pushProfile: pushProfile,
     getProfile: getProfile,
     getSession: getSession,
-    getUserAverage: getUserAverage
+    getUserAverage: getUserAverage,
+    getUserAverageDate: getUserAverageDate
   }
--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,16 @@ app.get("/session/:pid", (req, res) => {
         dbQuery.getSession(req,res)
       });
 app.get("/session/usersavg/:leg/:pid", (req, res) => {
-        console.log(req.params.pid + req.params.date)
+        // console.log(req.params.pid + req.params.date)
         dbQuery.getUserAverage(req,res)
         .then((resp)=>res.status(200).json(resp))
         .catch((err)=>res.status(400).json(err))
       });
-app.get("/session/usersavg/:pid/:date", (req, res) => {
-        console.log(req.params.pid + req.params.date)
+app.get("/session/usersavg/:leg/:pid/:date", (req, res) => {
+        // console.log(req.params.pid + req.params.date)
         dbQuery.getUserAverageDate(req,res)
+        .then((resp)=>res.status(200).json(resp))
+        .catch((err)=>res.status(400).json(err))
       });
 app.get("/session/:pid", (req, res) => {
         console.log(req.params.pid)
