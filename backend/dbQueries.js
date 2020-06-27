979489345b950e95b65d7e33151f367f8e29b7ea
Saravanan <jvs.ravanan@gmail.com>
2020-06-27 20:30:21 +0530

    backend wrap(..I guess)

--- backend/dbQueries.js
+++ backend/dbQueries.js
@@ -25,7 +25,7 @@ let pushProfile = function(req,res) {
       console.log('connected')
     }
   })
-  client.query("insert into profile(age, height, weight, disability_info, name) values ($1,$2,$3,$4,$5);",[req.body.age, req.body.height, req.body.weight, req.body.disability_info, req.body.name])
+  client.query("insert into profile(age, height, weight, disability_info, name, unique_id) values ($1,$2,$3,$4,$5,$6);",[req.body.age, req.body.height, req.body.weight, req.body.disability_info, req.body.name, req.body.unique_id])
   .then((response)=>{res.status(200).json({message: "profile created"}); client.end()})
   .catch((error)=>{res.status(400).json(error); console.log(error); client.end()})
 }
@@ -187,14 +187,14 @@ async function getPeakData(req,res) {
 }
 
 module.exports={
-    pushSession:pushSession,
-    pushProfile: pushProfile,
-    getProfile: getProfile,
-    getSession: getSession,
-    getUserAverage: getUserAverage,
-    getUserAverageDate: getUserAverageDate,
-    endSession: endSession,
-    currentVsAllAverage: currentVsAllAverage,
-    getCurrentSession: getCurrentSession,
-    getPeakData: getPeakData
+     pushSession,
+     pushProfile,
+     getProfile,
+     getSession,
+     getUserAverage,
+     getUserAverageDate,
+     endSession,
+     currentVsAllAverage,
+     getCurrentSession,
+     getPeakData
   }
